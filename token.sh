21ef9b4b748fe2815d14a0a06167bceff91bf634
Shreyas Gandhi <100945644+ShreyasGandhi0607@users.noreply.github.com>
2026-01-27 17:41:43 +0530

    Update token.sh

--- token.sh
+++ token.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
 set -e
 
-# Refresh token validity
 EXPIRY_DAYS=30
-REMIND_DAYS=(9 7 5 3 1)
+REMIND_DAYS=(21 7 5 3 1)
 
-# Convert creation date to epoch
+if [[ -z "$REFRESH_TOKEN_CREATED_AT" ]]; then
+  echo "REFRESH_TOKEN_CREATED_AT is not set"
+  exit 1
+fi
+
 CREATED_EPOCH=$(date -u -d "$REFRESH_TOKEN_CREATED_AT" +%s)
 NOW_EPOCH=$(date -u +%s)
 
-# Calculate days passed and left
-DAYS_PASSED=$(( (NOW_EPOCH - CREATED_EPOCH) / 86400 ))
+if (( CREATED_EPOCH > NOW_EPOCH )); then
+  DAYS_PASSED=0
+else
+  DAYS_PASSED=$(( (NOW_EPOCH - CREATED_EPOCH) / 86400 ))
+fi
+
 DAYS_LEFT=$(( EXPIRY_DAYS - DAYS_PASSED ))
 
-# Export for other scripts
 export DAYS_LEFT
 export SHOULD_NOTIFY="false"
 
 for d in "${REMIND_DAYS[@]}"; do
   if [[ "$DAYS_LEFT" -eq "$d" ]]; then
-    export SHOULD_NOTIFY="true"
+    SHOULD_NOTIFY="true"
+    break
   fi
 done
+
+export SHOULD_NOTIFY
 
 echo "Refresh token days left: $DAYS_LEFT"
